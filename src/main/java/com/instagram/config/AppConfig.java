0f93f5a4bfd8f7c4fe974b06f72aa2dca6eef149
kimsukkyung0 <smk93021@gmail.com>
2024-06-25 22:16:14 +0900

    (5/12) 17:38  - spring security추가  - jwt 토큰 추가

---
+++ src/main/java/com/instagram/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.instagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain securityConfiguration(HttpSecurity http) throws Exception{
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST,"/signup").authenticated())
+                .addFilterAfter(null,null)
+                .addFilterBefore(null,null)
+                .csrf().disable()
+                .formLogin().and().httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/instagram/config/SecurityContext.java
@@ -0,0 +1,6 @@
+package com.instagram.config;
+
+public class SecurityContext {
+    public static final String JWT_KEY = "Donghae0Mul--InstAgram1049fj";
+    public static final String HEADER = "Authentication";
+}
